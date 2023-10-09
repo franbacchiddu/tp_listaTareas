535539e540d3dfc09b432232b74c197dccb62b08
VirginiaNM <virginia.n.martinez@gmail.com>
2023-10-09 10:17:11 -0300

    Clase tarea. Fix historial logout

---
+++ login/ClaseTarea.php
@@ -0,0 +1,33 @@
+<?php
+class ClaseTarea {
+    private $conn;
+
+    public function __construct($db) {
+        $this->conn = $db->getConnection();
+    }
+
+    public function agregarTarea($tarea, $completado, $fecha, $categoria, $nombre_usuario) {
+        $stmt = $this->conn->prepare("INSERT INTO tareas (tarea, completado, fecha, categoria, nombre_usuario) VALUES (?, ?, ?, ?, ?)");
+        return $stmt->execute([$tarea, $completado, $fecha, $categoria, $nombre_usuario]);
+    }
+
+    public function obtenerTareasPorUsuario($nombre_usuario) {
+        $stmt = $this->conn->prepare("SELECT * FROM tareas WHERE nombre_usuario = ?");
+        $stmt->execute([$nombre_usuario]);
+        return $stmt->fetchAll();
+    }
+
+    public function obtenerTareaPorNombreUsuario($nombre_usuario, $nombre_tarea) {
+        $stmt = $this->conn->prepare("SELECT * FROM tareas WHERE nombre_usuario = ? AND tarea = ?");
+        $stmt->execute([$nombre_usuario, $nombre_tarea]);
+        return $stmt->fetch();
+    }
+
+    public function obtenerTareasPorNombreUsuario($nombre_usuario) {
+        $stmt = $this->conn->prepare("SELECT * FROM tareas WHERE nombre_usuario = ?");
+        $stmt->execute([$nombre_usuario]);
+        return $stmt->fetchAll();
+    }
+}
+?>
+
